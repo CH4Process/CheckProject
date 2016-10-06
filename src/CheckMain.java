ff2cbe1d9b65bf77d2c817a454d1580407c25537
Guillaume CH4 <guillaume.bataille@ch4process.fr>
2016-10-07 00:33:13 +0200

    CH4P_checkProject nouvelle méthode
    création de méthode
    nouvelle fonction restartProcess à debugger
    amélioration du code

--- src/CheckMain.java
+++ src/CheckMain.java
@@ -6,15 +6,40 @@ import java.net.Socket;
 import java.util.ArrayList;
  
 public class CheckMain {
+	static java.lang.Process p;
+	static ArrayList processList = new ArrayList();
+	static boolean vpnIsRunning;
 
 	public static void main(String[] args) throws IOException {
-		ArrayList processList = new ArrayList();
+	//	ArrayList processList = new ArrayList();
+	//	java.lang.Process p;
+	
+		getOnProcessList();
+
+		// Affichage de l'ensemble des processu récupérés
+		System.out.println("contenu de processList : " + processList);
+		
+		isProcessRunning();
+		
+		if (vpnIsRunning) {
+			System.out.println("le tunnel VPN est actif");
+			// Si processus VPN actif, vérifier la connexion : envoyer signal test et attendre la réponse
+			// 		Si ok, fin du test
+			pingInternalServer();
+		}
+		else {
+			// exec: Executes the specified string command in a separate process.
+			// N tentative de relance du tunnel VPN : runtime ?
+			   System.out.println("Le tunnel VPN ne fait pas partie de la liste des processus");
+			   restartProcess();
+		}
+		
+	}
+	private static void getOnProcessList(){
+	//	java.lang.Process p;
+		String carASup = "\"";
 		String processDetail [];
 		int nomP = 0;
-		java.lang.Process p;
-		String processCible = "notepad.exe";
-		String carASup = "\"";
-		
 		try {
 			// process : recois la chaine de caractère retournée par getRuntime
 			String process;
@@ -26,8 +51,8 @@ public class CheckMain {
 			// Process p = Runtime.getRuntime().exec("ps -few");
 			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			while ((process = input.readLine()) != null) {
-				System.out.println("Processus : " + process); // <-- Print all Process here line
-												// by line
+				System.out.println("Processus : " + process); 	// <-- Print all Process here line
+																// by line
 				process = process.replaceAll(carASup,"");
 				// processDetail : tableau à une dimension qui recoit la chaine de caractère process séparée selon le caractère ','
 				processDetail = process.split(",");
@@ -35,55 +60,67 @@ public class CheckMain {
 				System.out.println("Nom du processus : " + processDetail[nomP]);
 				//processList : Array list qui regroupe l'ensemble des nomP
 				processList.add(processDetail[nomP]);
- 			}
+			}
 			input.close();
 		} catch (Exception err) {
 			err.printStackTrace();
 		}
-		// Affichage de l'ensemble des processu récupérés
-		System.out.println("contenu de processList : " + processList);
+	}
+	private static boolean isProcessRunning (){
+		String processCible = "notepad.exe";
 		// controle que le process processCible fait bien partie de la liste
-		boolean vpnIsRunning = processList.contains(processCible);
-		if (vpnIsRunning) {
-			System.out.println("le tunnel VPN est actif");
-			// Si processus VPN actif, vérifier la connexion : envoyer signal test et attendre la réponse
-			// 		Si ok, fin du test
-			Socket socketInt = new Socket();
-			InetSocketAddress addressInt = new InetSocketAddress("ch4pcsup.ddns.net",1197);  
+		return vpnIsRunning = processList.contains(processCible);		
+	}
+	private static void pingInternalServer(){
+		Socket socketInt = new Socket();
+		InetSocketAddress addressInt = new InetSocketAddress("ch4pcsup.ddns.net",1197);  
+		try {
+			socketInt.connect(addressInt,2000);
+			System.out.println("connexion à CH4pcsup via VPN réussie");
+		//		Si KO :
+		} catch (Exception eInt) {
+			System.out.println("échec de connexion à CH4pcsup via VPN");
+		//		SI serveur à retourner un anomalie spécifique ?
+		//		SINON : test connexion :
+			pingExternalServer();
+		} finally {
+			try {socketInt.close();}
+			catch (Exception eInt) {}
+		}
+	}
+	private static void pingExternalServer() {
+		Socket socketExt = new Socket();
+		InetSocketAddress addressExt = new InetSocketAddress("www.google.com",80);  
+		try {
+			socketExt.connect(addressExt,2000);
+			System.out.println("connexion à www.google.fr réussi");
+			//test de connexion OK --> Le problème vient du serveur ou du logiciel VPN
+			// arret relance du processus
+		} catch (Exception eExt) {
+			System.out.println("échec de connexion à www.google.fr");
+			// test de connexion KO --> connexion internet perdue
+			// redemarre dongle USB ?
+		} finally {
+			try {socketExt.close();}
+			catch (Exception eExt) {}
+		}
+	}
+	private static void restartProcess(){
+		int nbreMaxRestart = 0;
+		while (!vpnIsRunning && nbreMaxRestart < 5) {
+			nbreMaxRestart ++;
 			try {
-				socketInt.connect(addressInt,2000);
-				System.out.println("connexion à CH4pcsup via VPN réussie");
-			//		Si KO :
-			} catch (Exception eInt) {
-				System.out.println("échec de connexion à CH4pcsup via VPN");
-			//		SI serveur à retourner un anomalie spécifique ?
-			//		SINON : test connexion :
-				Socket socketExt = new Socket();
-				InetSocketAddress addressExt = new InetSocketAddress("www.google.com",80);  
-				try {
-					socketExt.connect(addressExt,2000);
-					System.out.println("connexion à www.google.fr réussi");
-					//test de connexion OK --> Le problème vient du serveur ou du logiciel VPN
-					// arret relance du processus
-				} catch (Exception eExt) {
-					System.out.println("échec de connexion à www.google.fr");
-					// test de connexion KO --> connexion internet perdue
-					// redemarre dongle USB ?
-				} finally {
-					try {socketExt.close();}
-					catch (Exception eExt) {}
-				}
-			} finally {
-				try {socketInt.close();}
-				catch (Exception eInt) {}
+				p = Runtime.getRuntime().exec("notepad.exe");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		} 
+		if (nbreMaxRestart >= 5) {
+			System.out.println("Nombre maximum de tentatvive de redémarrage du processus atteint !!!");
 		}
 		else {
-			// exec: Executes the specified string command in a separate process.
-			// N tentative de relance du tunnel VPN : runtime ?
-			   System.out.println("Le tunnel VPN ne fait pas partie de la liste des processus");
-			   p = Runtime.getRuntime().exec("notepad.exe");
+			System.out.println("Le nombre de tentative de redémarrage du processus est de : "+nbreMaxRestart);
 		}
-		
 	}
 }
